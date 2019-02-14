9ebf6e71b76604488f8cd9a7dbc5ad0180900688
Amol Chandrashekhar Puranik (Digital) <AM20064429@wipro.com>
2019-02-14 10:00:34 +0530

    5

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,64 @@
+package io.pivotal.pal.tracker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+    private HashMap<Long, TimeEntry> timeEntries = new HashMap<>();
+
+    private long currentId = 1L;
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        Long id = 0L;
+        if(timeEntry.getId()>0)
+            id=timeEntry.getId();
+
+        else
+            id = currentId++;
+
+
+        TimeEntry newTimeEntry = new TimeEntry(
+                id,
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                timeEntry.getDate(),
+                timeEntry.getHours()
+        );
+
+        timeEntries.put(id, newTimeEntry);
+        return newTimeEntry;
+    }
+
+    @Override
+    public TimeEntry find(Long id) {
+        return timeEntries.get(id);
+    }
+
+    @Override
+    public List<TimeEntry> list() {
+        return new ArrayList<>(timeEntries.values());
+    }
+
+    @Override
+    public TimeEntry update(Long id, TimeEntry timeEntry) {
+        if (find(id) == null) return null;
+
+        TimeEntry updatedEntry = new TimeEntry(
+                id,
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                timeEntry.getDate(),
+                timeEntry.getHours()
+        );
+
+        timeEntries.replace(id, updatedEntry);
+        return updatedEntry;
+    }
+
+    @Override
+    public void delete(Long id) {
+        timeEntries.remove(id);
+    }
+}
